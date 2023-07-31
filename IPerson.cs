b911a141a437d507a6edec75f0e3d6472a7bc23c
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-07-31 19:20:23 +0530

    first commit

---
+++ IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section10_Interface
+{
+    public interface IPerson
+    {
+        string getInfo();
+        string getTypeOfPerson();
+    }
+
+}
